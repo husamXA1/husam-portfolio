76d9b6dc62b7f6e94a243ad60a2332fda7ba4789
husamXA1 <husamxm0@gmail.com>
2025-03-16 07:48:44 +0200

    Add Hero component

---
+++ app/components/Hero.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function Hero() {
+  return (
+    <section className="py-20">
+      <div className="container flex max-md:flex-col gap-10 md:justify-between md:items-center">
+        <div className="w-full text-center flex flex-col gap-2">
+            <h1 className="text-2xl lg:text-3xl font-bold">Hello, I'm <span className="bg-black px-1 text-[--theme-color]">Husam</span></h1>
+            <p className="text-lg lg:text-xl">A Web Developer</p>
+            <div className="flex gap-2 justify-center">
+                <Link className="btn btn-primary" href="#about">About me</Link>
+                <Link className="btn" href="#contact">Get in touch</Link>
+            </div>
+        </div>
+        <div className="w-full">
+            <img src="/images/hello.svg" alt="hello" />
+        </div>
+      </div>
+    </section>
+  );
+}
