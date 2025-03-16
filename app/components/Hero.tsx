9091631516836c722547722f4a6031c0b43cb6d2
husamXA1 <husamxm0@gmail.com>
2025-03-16 14:28:22 +0200

    Update button spacing in Hero component for better layout

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
         <div className="w-full text-center flex flex-col gap-2">
             <h1 className="text-2xl lg:text-3xl font-bold">Hello, I&apos;m <span className="bg-black px-1 text-[--theme-color]">Husam</span></h1>
             <p className="text-lg lg:text-xl">A Web Developer</p>
-            <div className="flex gap-2 justify-center">
+            <div className="flex gap-4 justify-center">
                 <Link className="btn btn-primary" href="#about">About me</Link>
                 <Link className="btn" href="#contact">Get in touch</Link>
             </div>
