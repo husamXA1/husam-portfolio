d90d92bd2956911a93b3de8f1dcfc34c9da60b82
husamXA1 <husamxm0@gmail.com>
2025-03-16 14:29:33 +0200

    Add IDs to About and Hero sections, and enable smooth scrolling

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function About() {
   return (
-    <section className="bg-[--theme-color] py-20">
+    <section className="bg-[--theme-color] py-20" id="about">
       <div className="container flex max-md:flex-col gap-10 items-center">
         <div className="w-full">
           <Image
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <section className="py-20">
+    <section className="py-20" id="hero">
       <div className="container flex max-md:flex-col gap-10 md:justify-between md:items-center">
         <div className="w-full text-center flex flex-col gap-2">
             <h1 className="text-2xl lg:text-3xl font-bold">Hello, I&apos;m <span className="bg-black px-1 text-[--theme-color]">Husam</span></h1>
