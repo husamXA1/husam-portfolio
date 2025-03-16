a2ec6186051b4adb06907542a511b650196fbdd8
husamXA1 <husamxm0@gmail.com>
2025-03-16 14:15:17 +0200

    Refactor Header component for improved styling

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,13 +5,18 @@ import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 export default function Header() {
-
-    const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
   return (
-    <header className="bg-[#f3d129] text-gray-800 border-b-[5px] border-b-gray-800">
+    <header className="bg-[#f3d129] text-black border-b-[5px] border-b-black">
       <div className="container flex justify-between items-center py-4">
-        <Link href="#"><h1 className="font-bold text-xl">Husam</h1></Link>
-        <nav className={`md:flex ${!showMenu && "hidden"} max-md:absolute top-0 left-0 max-md:w-full max-md:h-screen max-md:bg-[#f3d129] flex justify-center items-center`}>
+        <Link href="#">
+          <h1 className="font-semibold text-xl">Husam</h1>
+        </Link>
+        <nav
+          className={`md:flex ${
+            !showMenu && "hidden"
+          } max-md:absolute top-0 left-0 max-md:w-full max-md:h-screen max-md:bg-[#f3d129] flex justify-center items-center`}
+        >
           <ul className="md:flex gap-5">
             <li>
               <Link href="#about">About</Link>
@@ -26,12 +31,15 @@ export default function Header() {
               <Link href="#contact">Contact</Link>
             </li>
           </ul>
-          <button className="md:hidden absolute top-5 right-5" onClick={() => setShowMenu(false)}>
+          <button
+            className="md:hidden absolute top-5 right-5"
+            onClick={() => setShowMenu(false)}
+          >
             <FaTimes size={24} />
           </button>
         </nav>
         <button className="md:hidden" onClick={() => setShowMenu(true)}>
-            <FaBars size={24} />
+          <FaBars size={24} />
         </button>
       </div>
     </header>
