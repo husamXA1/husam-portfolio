b9b2659011c7392c8f0de23e0daa03855312f989
husamXA1 <husamxm0@gmail.com>
2025-03-30 05:57:15 +0200

    Add Footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black text-white py-4">
+      <div className="container mx-auto text-center">
+        <p className="text-sm">&copy; 2025 Husam. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Footer from "./components/Footer";
 import Header from "./components/Header"
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Skills />
       <Projects />
+      <Footer />
     </main>
   );
 }
